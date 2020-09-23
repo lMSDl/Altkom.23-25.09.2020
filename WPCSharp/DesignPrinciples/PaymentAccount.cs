1b06cd2b39e30e5bb7da28ff6f48f5bf5140a544
Paweł Łęczycki <pawel@msd.info.pl>
2020-09-23 10:16:13 +0200

    KISS

---
+++ WPCSharp/DesignPrinciples/PaymentAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.DesignPrinciples
+{
+    public class PaymentAccount
+    {
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public float Incomes { get; set; }
+        public float Outcomes { get; set; }
+        public float AllowedDebit { get; set; }
+
+        public PaymentAccount(int id)
+        {
+            Id = id;
+        }
+
+        public bool HasDebit => Incomes - Outcomes < 0;
+    }
+
+}
---
+++ WPCSharp/DesignPrinciples/PaymentService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.DesignPrinciples
+{
+    public class PaymentService
+    {
+        private ICollection<PaymentAccount> PaymentAccounts { get; } = new List<PaymentAccount> { new PaymentAccount(1), new PaymentAccount(2) };
+
+        public bool DeleteCustomer (PaymentAccount param)
+        {
+            return PaymentAccounts.Remove(param);
+        }
+
+        public bool Charge(int paymentAccountId, float amonut)
+        {
+            var paymentAccount = PaymentAccounts.SingleOrDefault(x => x.Id == paymentAccountId);
+            if (paymentAccount == null)
+                return false;
+            if (paymentAccount.Incomes - paymentAccount.Outcomes + paymentAccount.AllowedDebit < amonut)
+                return false;
+
+            paymentAccount.Outcomes += amonut;
+            return true;
+        }
+
+        public void Fund(int paymentAccountId, float amonut)
+        {
+            var paymentAccount = PaymentAccounts.SingleOrDefault(x => x.Id == paymentAccountId);
+            if (paymentAccount == null)
+                return;
+            paymentAccount.Incomes += amonut;
+        }
+        
+        public float? GetBalance(int paymentAccountId)
+        {
+            var customer = PaymentAccounts.Where(x => x.Id == paymentAccountId).SingleOrDefault();
+            return customer?.Incomes - customer?.Outcomes;
+        }
+    }
+}

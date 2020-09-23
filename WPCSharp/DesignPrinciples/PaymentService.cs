025922122fc1195bbae140065043f9de72549bdd
Paweł Łęczycki <pawel@msd.info.pl>
2020-09-23 11:02:09 +0200

    DEMETER

--- WPCSharp/DesignPrinciples/PaymentService.cs
+++ WPCSharp/DesignPrinciples/PaymentService.cs
@@ -8,19 +8,19 @@ namespace WPCSharp.DesignPrinciples
 {
     public class PaymentService
     {
-        public bool Charge(Customer customer, float amount)
+        public bool Charge(PaymentAccount paymentAccount, float amount)
         {
-            if (customer.PaymentAccount == null)
+            if (paymentAccount == null)
                 return false;
 
-            return customer.PaymentAccount.Charge(amount);
+            return paymentAccount.Charge(amount);
         }
 
-        public void Fund(Customer customer, float amount)
+        public void Fund(PaymentAccount paymentAccount, float amount)
         {
-            if (customer.PaymentAccount == null)
+            if (paymentAccount == null)
                 return;
-            customer.PaymentAccount.Fund(amount);
+            paymentAccount.Fund(amount);
         }
     }
 }
